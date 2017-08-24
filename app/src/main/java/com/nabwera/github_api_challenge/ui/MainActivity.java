43cf29f55df9f76d98c8b14690314413654f7c03
Roger Taracha <rojtaracha@gmail.com>
2017-08-24 17:29:39 +0300

    [FEATURE] Prepare ListView used to display data: [+] Update corresponding class and xml file.

--- app/src/main/java/com/nabwera/github_api_challenge/ui/MainActivity.java
+++ app/src/main/java/com/nabwera/github_api_challenge/ui/MainActivity.java
@@ -2,14 +2,19 @@ package com.nabwera.github_api_challenge.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.nabwera.github_api_challenge.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.list_item_users_text);
     }
 }
