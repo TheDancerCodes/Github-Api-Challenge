40df96a40f10f9dbb4e1fee96f6e88d4deb55b0d
Roger Taracha <rojtaracha@gmail.com>
2017-08-24 14:44:27 +0300

    [FEATURE] Feature Separation By Feature: [+] Structuring the application by package per feature.

--- app/src/main/java/com/nabwera/github_api_challenge/ui/MainActivity.java
+++ app/src/main/java/com/nabwera/github_api_challenge/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.nabwera.github_api_challenge;
+package com.nabwera.github_api_challenge.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.nabwera.github_api_challenge.R;
 
 public class MainActivity extends AppCompatActivity {
 
