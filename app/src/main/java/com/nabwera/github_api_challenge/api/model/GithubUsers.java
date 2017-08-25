8cd57ed90ab750cfa51f7dddfb4136a57ef289e6
Roger Taracha <rojtaracha@gmail.com>
2017-08-25 10:27:22 +0300

    [FEATURE] Refactor GithubUsers class & Create a POJO: [+] POJO holds all fields. [+] Provides getter and setter methods for fields that we are expecting in the JSON response object.

--- app/src/main/java/com/nabwera/github_api_challenge/api/model/GithubUsers.java
+++ app/src/main/java/com/nabwera/github_api_challenge/api/model/GithubUsers.java
@@ -1,25 +1,74 @@
 package com.nabwera.github_api_challenge.api.model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by nabwera on 24/08/2017.
  */
 
 public class GithubUsers {
 
-    private String name;
+//    private String login;
 //    private String profile_photo;
 //    private String profile_url;
 
+//    public String getName() {
+//        return login;
+//    }
 
-    public String getName() {
-        return name;
+    @SerializedName("login")
+    @Expose
+    private String login;
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("avatar_url")
+    @Expose
+    private String avatarUrl;
+    @SerializedName("url")
+    @Expose
+    private String url;
+
+
+    public String getLogin() {
+        return login;
     }
 
-//    public String getProfile_photo() {
-//        return profile_photo;
-//    }
-//
-//    public String getProfile_url() {
-//        return profile_url;
-//    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+
+    public String getUrl() {
+        return url;
+    }
+
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+
 }
