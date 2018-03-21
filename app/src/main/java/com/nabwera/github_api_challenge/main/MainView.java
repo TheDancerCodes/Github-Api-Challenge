9e136d1fd298da2d06c92cf1576ac728e83dbfef
TheDancerCodes <rojtaracha@gmail.com>
2018-03-21 15:57:50 +0300

    [FEATURE] Add MainView
    [+] The methods in MainView will interact with view layer to update user interface as needed.
    
    [+] MainView will be implemented by MainActivity.

---
+++ app/src/main/java/com/nabwera/github_api_challenge/main/MainView.java
@@ -0,0 +1,29 @@
+package com.nabwera.github_api_challenge.main;
+
+import com.nabwera.github_api_challenge.base.BaseView;
+import com.nabwera.github_api_challenge.model.GithubUsers;
+
+import java.util.List;
+
+/**
+ * Created by TheDancerCodes on 21/03/2018.
+ */
+
+public interface MainView extends BaseView {
+
+    // The methods below will interact with view layer to update user interface as needed.
+
+    /**
+     * show users
+     *
+     * @param githubUsersList : show list of users
+     */
+    void showUsers(List<GithubUsers> githubUsersList);
+
+    /**
+     * show network error
+     *
+     * @param error : error message
+     */
+    void showNetworkError(String error);
+}
