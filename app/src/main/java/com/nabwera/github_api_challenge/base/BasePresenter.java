5b8b5d6b45335b1ce762b9558f051c1c9749b226
TheDancerCodes <rojtaracha@gmail.com>
2018-03-21 14:31:45 +0300

    [FEATURE] Create base package:
    [+] This package contains the BasePresenter & BaseView.

---
+++ app/src/main/java/com/nabwera/github_api_challenge/base/BasePresenter.java
@@ -0,0 +1,9 @@
+package com.nabwera.github_api_challenge.base;
+
+/**
+ * Created by TheDancerCodes on 21/03/2018.
+ */
+
+public interface BasePresenter {
+
+}
---
+++ app/src/main/java/com/nabwera/github_api_challenge/base/BaseView.java
@@ -0,0 +1,20 @@
+package com.nabwera.github_api_challenge.base;
+
+/**
+ * Created by TheDancerCodes on 21/03/2018.
+ */
+
+public interface BaseView {
+
+    /**
+     * Display Progress View
+     */
+    void showProgress();
+
+
+    /**
+     * Hide Progress View
+     */
+    void hideProgress();
+
+}
