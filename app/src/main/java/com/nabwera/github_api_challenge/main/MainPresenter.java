8c17e3f8edd0c5f9b20241ab93b10b3e38865415
TheDancerCodes <rojtaracha@gmail.com>
2018-03-21 17:12:43 +0300

    [FEATURE] Add MainPresenter
    [+] MainPresenter class contains method to fetch users.

---
+++ app/src/main/java/com/nabwera/github_api_challenge/main/MainPresenter.java
@@ -0,0 +1,13 @@
+package com.nabwera.github_api_challenge.main;
+
+/**
+ * Created by TheDancerCodes on 21/03/2018.
+ */
+
+public interface MainPresenter {
+
+    /**
+     * fetch users
+     */
+    void onFetchUsers();
+}
